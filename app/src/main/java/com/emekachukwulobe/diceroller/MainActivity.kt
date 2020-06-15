9483504e10db78e147e19b0699a0846300dc8227
Emeka Chukwulobe <emekabe@yahoo.com>
2020-06-15 12:20:00 +0100

    Finished the main app requirements

--- app/src/main/java/com/emekachukwulobe/diceroller/MainActivity.kt
+++ app/src/main/java/com/emekachukwulobe/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -23,11 +24,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        var resultText: TextView = findViewById(R.id.result_text)
+        var diceImage: ImageView = findViewById(R.id.dice_image)
 
         val randomInt = Random.nextInt(6) + 1
 
-        resultText.text = randomInt.toString()
+        var diceImageResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
+        diceImage.setImageResource(diceImageResource)
     }
 }
